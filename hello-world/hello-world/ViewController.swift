8e6ebc5e9277d668f8828b0a5ab4d768eee1e0dc
Berk Cohadar <berkcohadar1@gmail.com>
2021-09-12 17:40:17 -0400

    buttons bug fixed

--- hello-world/hello-world/ViewController.swift
+++ hello-world/hello-world/ViewController.swift
@@ -11,6 +11,7 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var imageView: UIImageView!
     @IBOutlet weak var colorButton: UIButton!
+    @IBOutlet weak var changeButton: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
