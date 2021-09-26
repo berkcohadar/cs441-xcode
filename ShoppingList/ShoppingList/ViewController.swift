2b698390a35f27fd92ea955a14609ec004b16ddc
Berk Cohadar <berkcohadar1@gmail.com>
2021-09-26 18:44:09 -0400

    Shopping List updated. Item search onClick function added. Share button added. No items view created.

--- ShoppingList/ShoppingList/ViewController.swift
+++ ShoppingList/ShoppingList/ViewController.swift
@@ -6,60 +6,78 @@
 //
 
 import UIKit
+import Social
 
 class ViewController: UIViewController {
 
     @IBOutlet weak var TableList: UITableView!
     @IBOutlet weak var AddButton: UIButton!
-    @IBOutlet weak var InputArea: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         TableList.delegate = self
         TableList.dataSource = self
     }
-    var tasks:[String] = [];
+    var shoppingList:[String] = [];
     @IBAction func addToTable(_ sender: Any) {
-        if InputArea.text != ""{
-            tasks.append(InputArea.text!)
-            TableList.beginUpdates()
-            TableList.insertRows(at: [IndexPath(row: tasks.count-1, section: 0)], with: .automatic)
-            TableList.endUpdates()
-            InputArea.text = ""
-        }
-        else{
-            showInputDialog(title: "Add task",
-                            subtitle: "Please enter a new task below.",
-                            actionTitle: "Add",
-                            cancelTitle: "Cancel",
-                            inputPlaceholder: "New task",
-                            inputKeyboardType: .phonePad,
-                            actionHandler:
-                                    { (input:String?) in
-                                        self.tasks.append(input!)
-                                        self.TableList.beginUpdates()
-                                        self.TableList.insertRows(at: [IndexPath(row: self.tasks.count-1, section: 0)], with: .automatic)
-                                        self.TableList.endUpdates()
-                                    })
-        }
+        takeInput(title: "Add to List",
+                  message: "Please enter an item below.",
+                        actionTitle: "Add",
+                        cancelTitle: "Cancel",
+                        placeholder: "New item",
+                        actionHandler:
+                          { (input:String?) in
+                            self.shoppingList.append(input!)
+                            self.TableList.beginUpdates()
+                            self.TableList.insertRows(at: [IndexPath(row: self.shoppingList.count-1, section: 0)], with: .automatic)
+                            self.TableList.endUpdates()
+                          }
+                        )
     }
-    
+    @IBAction func shareTwitter(_ sender: Any) {
+        let activityViewController = UIActivityViewController(activityItems: shoppingList, applicationActivities: nil)
+        activityViewController.popoverPresentationController?.sourceView = self.view
+        self.present(activityViewController, animated: true, completion: nil)
+    }
 }
+extension UITableView {
+	
+    func emptyScreen(_ text: String) { // Will be called when there is no item in the "shoppingList" array.
+        let emptyMessage = UILabel(frame: CGRect(x: 0, y: 0, width: self.bounds.size.width, height: self.bounds.size.height))
+        emptyMessage.text = text
+        emptyMessage.textColor = .black
+        emptyMessage.numberOfLines = 0
+        emptyMessage.textAlignment = .center
+        emptyMessage.font = UIFont(name: "System", size: 16)
+        //emptyMessage.sizeToFit()
+
+        // background change down
+        self.backgroundView = emptyMessage
+        self.separatorStyle = .none
+    }
+
+    func restore() { // Will be called when item is added.
+        // background change down. reset to default (nil)
+        self.backgroundView = nil
+        self.separatorStyle = .singleLine
+    }
+}
+
 extension UIViewController {
-    func showInputDialog(title:String? = nil,
-                         subtitle:String? = nil,
-                         actionTitle:String? = "Add",
-                         cancelTitle:String? = "Cancel",
-                         inputPlaceholder:String? = nil,
-                         inputKeyboardType:UIKeyboardType = UIKeyboardType.default,
+    func takeInput(title:String?,
+                         message:String?,
+                         actionTitle:String?,
+                         cancelTitle:String?,
+                         placeholder:String?,
                          cancelHandler: ((UIAlertAction) -> Swift.Void)? = nil,
                          actionHandler: ((_ text: String?) -> Void)? = nil) {
+    
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
         
-        let alert = UIAlertController(title: title, message: subtitle, preferredStyle: .alert)
         alert.addTextField { (textField:UITextField) in
-            textField.placeholder = inputPlaceholder
-            textField.keyboardType = inputKeyboardType
+            textField.placeholder = placeholder
         }
+        
         alert.addAction(UIAlertAction(title: actionTitle, style: .default, handler: { (action:UIAlertAction) in
             guard let textField =  alert.textFields?.first else {
                 actionHandler?(nil)
@@ -73,8 +91,15 @@ extension UIViewController {
     }
 }
 extension ViewController: UITableViewDelegate{
+    func normalizeQuery(_ url:String) -> String {
+        return url.replacingOccurrences(of: " ", with: "+")
+    }
+    
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        print("tapped!")
+        let str: String = normalizeQuery("https://www.google.com/search?q="+shoppingList[indexPath[1]])
+        if let url = URL(string: str) {
+            UIApplication.shared.open(url)
+        }
     }
 }
 
@@ -84,17 +109,22 @@ extension ViewController: UITableViewDataSource{
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return tasks.count
+        if shoppingList.count == 0 {
+            tableView.emptyScreen("No items yet! Press button to start.")
+        } else {
+            tableView.restore()
+        }
+        return shoppingList.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "cell",for:indexPath)
-        cell.textLabel?.text = tasks[indexPath.row]
+        cell.textLabel?.text = shoppingList[indexPath.row]
         return cell
     }
     func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
         if editingStyle == .delete {
-            tasks.remove(at: indexPath.row)
+            shoppingList.remove(at: indexPath.row)
             tableView.deleteRows(at: [indexPath], with: UITableView.RowAnimation.fade)
         }
     }
